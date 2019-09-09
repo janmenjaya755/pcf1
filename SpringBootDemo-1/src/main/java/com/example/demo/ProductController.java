a72d86ea27402f06931e176a7e9eb199166a2b3a
beher <beher@DESKTOP-2V0BI5G>
2019-09-09 22:19:43 +0530

    2nd controller push

--- SpringBootDemo-1/src/main/java/com/example/demo/ProductController.java
+++ SpringBootDemo-1/src/main/java/com/example/demo/ProductController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/amazon")
 public class ProductController {
 	
 	@GetMapping(value = "/prduct")
 	public String CreatProduct() {
-		return "welcome to boot";
+		return "welcome to springboot with pcf";
 	}
 
 }
