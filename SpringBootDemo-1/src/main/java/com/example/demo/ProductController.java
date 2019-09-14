fc94d1c5de5c3124e78b7d87c24800d0a589ce7e
beher <beher@DESKTOP-2V0BI5G>
2019-09-14 11:03:40 +0530

    this is my delete controller method push

--- SpringBootDemo-1/src/main/java/com/example/demo/ProductController.java
+++ SpringBootDemo-1/src/main/java/com/example/demo/ProductController.java
@@ -5,16 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/amazon")
+@RequestMapping(value = "/amazon/")
 public class ProductController {
 	
 	@GetMapping(value = "/prduct")
 	public String CreatProduct() {
-		return "welcome to springboot with pcf";
+		return "welcome to springboot with pcf cloud hiiiiiiiiiiiiii";
 	}
 	@GetMapping(value = "/product")
 	public String getProduct() {
-		return "welcome to springboot with pcf";
+		return "welcome to springboot with pcf i am in marlabs.hellllllllllllooooo";
+	}
+	public void delete() {
+		
 	}
 	
 
